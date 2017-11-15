392df468cfe21ccd4b76fdc1ed3bd0af1e50b99e
tdegeus <tom@geus.me>
2017-11-15 08:37:57 +0100

    added unstructured mesh

--- hdf5_xdmf/particles/example.py
+++ hdf5_xdmf/particles/example.py
@@ -8,11 +8,16 @@ x,y,z = np.meshgrid(np.linspace(0,10,10),np.linspace(0,10,10),np.linspace(0,10,1
 # convert the list of positions
 X = np.hstack(( x.reshape(-1,1), y.reshape(-1,1), z.reshape(-1,1) ))
 
+# create example radius
+R       = np.ones((X.shape[0]),dtype='float64')
+R[::2] *= 2.
+
 # open data file
 f = h5py.File('example.hdf5','w')
 
 # write particle positions, and a dummy connectivity
 f.create_dataset('/X',data=X)
+f.create_dataset('/R',data=R)
 f.create_dataset('/connectivity',data=np.arange(X.shape[0]))
 
 # create a sample deformation: simple shear
@@ -43,11 +48,16 @@ xmf = '''<?xml version="1.0" ?>
         example.hdf5:/X
         </DataItem>
       </Geometry>
+      <Attribute Name="Radius" AttributeType="Scalar" Center="Node">
+         <DataItem Dimensions="1000" NumberType="Float" Precision="8" Format="HDF">
+          example.hdf5:/R
+         </DataItem>
+      </Attribute>
       <Attribute Name="Displacement" AttributeType="Vector" Center="Node">
          <DataItem Dimensions="1000 3" NumberType="Float" Precision="8" Format="HDF">
           example.hdf5:/U/{inc:d}
          </DataItem>
-       </Attribute>
+      </Attribute>
     </Grid>
   </Domain>
 </Xdmf>
---
+++ hdf5_xdmf/unstructured_mesh/example.py
@@ -0,0 +1,74 @@
+
+import numpy as np
+import h5py
+
+# nodal positions
+coor = np.array([
+  [0., 0.], # 0
+  [1., 0.], # 1
+  [2., 0.], # 2
+  [0., 1.], # 3
+  [1., 1.], # 4
+  [2., 1.], # 5
+  [0., 2.], # 6
+  [1., 2.], # 7
+  [2., 2.], # 8
+])
+
+# connectivity (4-node quadrilaterals)
+conn = np.array([
+  [0,1,4,3],
+  [1,2,5,4],
+  [3,4,7,6],
+  [4,5,8,7],
+])
+
+# open data file
+f = h5py.File('example.hdf5','w')
+
+# write particle positions, and a dummy connectivity
+f.create_dataset('/coor',data=coor)
+f.create_dataset('/conn',data=conn)
+
+# create a sample deformation: simple shear
+for inc,gamma in enumerate(np.linspace(0,1,100)):
+
+  # - initialize displacement (should be 3-d for ParaView)
+  U = np.zeros((coor.shape[0],3),dtype='float64')
+  # - set
+  U[:,0] += gamma * coor[:,1]
+  # - store
+  f.create_dataset('/disp/%d'%inc,data=U)
+
+
+# ==================================================================================================
+
+xmf = '''<?xml version="1.0" ?>
+<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>
+<Xdmf Version="2.0">
+  <Domain>
+    <Grid Name="Time = {inc:d}">
+      <Topology TopologyType="Quadrilateral" NumberOfElements="4">
+        <DataItem Dimensions="4 4" Format="HDF">
+        example.hdf5:/conn
+        </DataItem>
+      </Topology>
+      <Geometry GeometryType="XY">
+        <DataItem Dimensions="9 2" Format="HDF">
+        example.hdf5:/coor
+        </DataItem>
+      </Geometry>
+      <Attribute Name="Displacement" AttributeType="Vector" Center="Node">
+         <DataItem Dimensions="9 3" NumberType="Float" Precision="8" Format="HDF">
+          example.hdf5:/disp/{inc:d}
+         </DataItem>
+      </Attribute>
+    </Grid>
+  </Domain>
+</Xdmf>
+'''
+
+
+for inc in range(100):
+
+  open('example_%03d.xmf'%inc,'w').write(xmf.format(inc=inc))
